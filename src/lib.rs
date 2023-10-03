b4ce939731ce90159062ce374278718f795d29c5
tiendang <tiendvlp@gmail.com>
2023-10-03 23:05:06 +0700

    Generate surreal custom statement

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 extern crate proc_macro;
 
+use proc_macro2::TokenStream;
+
 #[proc_macro_derive(surreal_derive)]
 pub fn create(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let input = proc_macro::TokenStream::from(input);
@@ -9,20 +11,45 @@ pub fn create(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
 
     let mut final_tokens = proc_macro2::TokenStream::new();
 
-    let field_convert_quote = ast.fields.iter().map(|field| {
+    let field_convert_btreemap_quote = ast.fields.iter().map(|field| {
         let field_name = &field.ident.as_ref().unwrap();
         return quote::quote! {
             map.insert("#field_name".to_owned(), surrealdb::sql::Value::from(self.#field_name.clone()));
         };
     });
 
+    let field_convert_set_expressions_quote: std::vec::Vec<TokenStream> = vec![("set", "Equal")].iter().map(|(fn_name, operator_name)| {
+        let fn_ident = syn::Ident::new(&format!("into_{}_expressions", fn_name), proc_macro2::Span::call_site());
+        let operator_ident = syn::Ident::new(operator_name, proc_macro2::Span::call_site());
+
+        let field_quotes = ast.fields.iter().map(|field| {
+            let field_name = &field.ident.as_ref().unwrap();
+            return quote::quote! {
+                vec.push((surrealdb::sql::Idiom::from("#field_name".to_owned()), surrealdb::sql::Operator::#operator_ident, surrealdb::sql::Value::from(self.#field_name)));
+            }
+        });
+
+        return quote::quote! {
+            pub fn #fn_ident(self) -> std::vec::Vec<(surrealdb::sql::Idiom, surrealdb::sql::Operator, surrealdb::sql::Value)> {
+               let mut vec: std::vec::Vec<(surrealdb::sql::Idiom, surrealdb::sql::Operator, surrealdb::sql::Value)> = std::vec::Vec::new();
+               #(#field_quotes)*
+
+               return vec;
+           }
+        };
+    }).collect();
+
     let gen = quote::quote! {
-        impl #struct_name {
-            pub fn into_btreemap(&self) -> std::collections::BTreeMap<String, surrealdb::sql::Value> {
+        impl std::convert::Into<std::collections::BTreeMap<String, surrealdb::sql::Value>> for #struct_name {
+            fn into(self) -> std::collections::BTreeMap<String, surrealdb::sql::Value> {
                 let mut map: std::collections::BTreeMap<String, surrealdb::sql::Value> = std::collections::BTreeMap::new();
-                #(#field_convert_quote)*
+                #(#field_convert_btreemap_quote)*
                 return map;
             }
+        }
+
+        impl #struct_name {
+           #(#field_convert_set_expressions_quote)*
         }
     };
 
