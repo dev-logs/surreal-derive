45993c587d0de98640012100cc6401f4fd8ed833
tiendangkobiton <tiendang@kobiton.com>
2023-10-07 20:55:14 +0700

    Fix some bug

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,57 @@
-pub mod surrealql_machine;
-
 extern crate proc_macro;
+use surreal_devl::macro_state::*;
+#[proc_macro]
+pub fn surreal_quote(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    let input = syn::parse_macro_input!(input as syn::LitStr);
+    let mut current_state= surreal_devl::macro_state::State::NORMAL;
+    let mut out_states: Vec<surreal_devl::macro_state::Content> = vec![];
 
+    for (i, c) in input.value().char_indices().into_iter() {
+        current_state = match &mut current_state {
+            surreal_devl::macro_state::State::NORMAL => match c {
+                '#' => State::MATCHING {
+                    current: Content::new(String::from(""), i as u32),
+                    traces: vec![Trace::NAKED]
+                },
+                _ => State::NORMAL,
+            },
+            State::MATCHING {ref mut current, ref mut traces } => match &(traces.last().unwrap()).consume(&c) {
+                true => {
+                    traces.pop();
+                    if traces.is_empty() {
+                        let mut matched_thing = current.clone();
+                        matched_thing.end = Some(i as u32);
+                        // State::MATCHED(matched_thing)
+                    }
 
+                    current_state
+                },
+                false => {
+                    current.merge_content(c.to_string());
+                    match c {
+                        '(' => traces.push(Trace::BRACKET),
+                        _ => {}
+                    }
+
+                    State::MATCHING {
+                        current: current.to_owned(), traces: traces.to_owned()
+                    }
+                }
+            },
+            _ => current_state
+        };
+
+        current_state = match current_state {
+            State::MATCHED(c) => {
+                out_states.push(c.clone());
+                State::NORMAL
+            },
+            _ => current_state
+        };
+    }
+
+    proc_macro::TokenStream::new()
+}
 
 #[proc_macro_derive(surreal_derive)]
 pub fn create(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
