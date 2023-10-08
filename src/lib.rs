38e75a224c2d44384052a0ad92b910c15ba242c0
tiendangkobiton <tiendang@kobiton.com>
2023-10-08 22:13:59 +0700

    Complete basic idiom logic for derive

--- src/lib.rs
+++ src/lib.rs
@@ -1,31 +1,37 @@
 extern crate proc_macro;
+
+use std::ops::Range;
+use quote::ToTokens;
 use surreal_devl::macro_state::*;
+
 #[proc_macro]
 pub fn surreal_quote(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    let input = syn::parse_macro_input!(input as syn::LitStr);
-    let mut current_state= surreal_devl::macro_state::State::NORMAL;
-    let mut out_states: Vec<surreal_devl::macro_state::Content> = vec![];
+    let input = syn::parse_macro_input!(input as syn::LitStr).value();
 
-    for (i, c) in input.value().char_indices().into_iter() {
+    let mut current_state = State::NORMAL;
+    let mut out_states: Vec<Content> = vec![];
+
+    for (i, c) in input.char_indices().into_iter() {
         current_state = match &mut current_state {
-            surreal_devl::macro_state::State::NORMAL => match c {
+            State::NORMAL => match c {
                 '#' => State::MATCHING {
                     current: Content::new(String::from(""), i as u32),
-                    traces: vec![Trace::NAKED]
+                    traces: vec![Trace::NAKED],
                 },
                 _ => State::NORMAL,
             },
-            State::MATCHING {ref mut current, ref mut traces } => match &(traces.last().unwrap()).consume(&c) {
+            State::MATCHING { ref mut current, ref mut traces } => match &(traces.last().expect("The traces must not be empty")).consume(&c) {
                 true => {
                     traces.pop();
-                    if traces.is_empty() {
-                        let mut matched_thing = current.clone();
-                        matched_thing.end = Some(i as u32);
-                        // State::MATCHED(matched_thing)
+                    match traces.is_empty() {
+                        true => {
+                            let mut matched_thing = current.clone();
+                            matched_thing.end = Some(i as u32);
+                            State::MATCHED(matched_thing)
+                        }
+                        false => current_state
                     }
-
-                    current_state
-                },
+                }
                 false => {
                     current.merge_content(c.to_string());
                     match c {
@@ -34,7 +40,8 @@ pub fn surreal_quote(input: proc_macro::TokenStream) -> proc_macro::TokenStream 
                     }
 
                     State::MATCHING {
-                        current: current.to_owned(), traces: traces.to_owned()
+                        current: current.to_owned(),
+                        traces: traces.to_owned(),
                     }
                 }
             },
@@ -45,90 +52,80 @@ pub fn surreal_quote(input: proc_macro::TokenStream) -> proc_macro::TokenStream 
             State::MATCHED(c) => {
                 out_states.push(c.clone());
                 State::NORMAL
-            },
+            }
             _ => current_state
         };
+    };
+
+    let mut out_str = input.clone();
+    for out_state in out_states {
+        out_str.replace_range(Range { start: out_state.start as usize, end: out_state.end.unwrap() as usize }, "{}");
     }
 
-    proc_macro::TokenStream::new()
+    (quote::quote! {
+        format!(#out_str)
+    }).into()
 }
 
 #[proc_macro_derive(surreal_derive)]
-pub fn create(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    let input = proc_macro::TokenStream::from(input);
+pub fn surreal_derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
 
     let ast: syn::ItemStruct = syn::parse_macro_input!(input as syn::ItemStruct);
     let struct_name = &ast.ident;
 
-    let field_convert_btreemap_quote = ast.fields.iter().map(|field| {
-        let field_name = &field.ident.as_ref().unwrap();
-        return quote::quote! {
-            map.insert("#field_name".to_owned(), surrealdb::sql::Value::from(self.#field_name.clone()));
-        };
+    let field_converters = ast.fields.iter().map(|field| {
+        let field_name = field.ident.as_ref().unwrap();
+        // Check if the field's type is Vec.
+        if let syn::Type::Path(type_path) = &field.ty {
+            if type_path.path.segments.first().unwrap().ident.to_string() == "Vec" {
+                    return quote::quote! {
+                        let mut array_value: std::vec::Vec<surrealdb::sql::Value> = self.#field_name.iter().map(|v| {
+                            surrealdb::sql::Value::from(v)
+                        })
+                        .collect();
+
+                        vec.push((
+                            surrealdb::sql::Idiom::from(stringify!(#field_name).to_owned()), // field name
+                            surrealdb::sql::Value::from(array_value)) // value
+                        );
+                    };
+                }
+        }
+
+        quote::quote! {
+            vec.push((
+                surrealdb::sql::Idiom::from(stringify!(#field_name).to_owned()), // field name
+                surrealdb::sql::Value::from(self.#field_name.clone())) // value
+            );
+        }
     });
 
-    let field_convert_set_expressions_quote: std::vec::Vec<proc_macro2::TokenStream> = vec![("create", "Equal"), ("update", "Equal")].iter().map(|(fn_name, operator_name)| {
-        let fn_ident = syn::Ident::new(&format!("into_{}_expressions", fn_name), proc_macro2::Span::call_site());
-        let operator_ident = syn::Ident::new(operator_name, proc_macro2::Span::call_site());
+    let into_idiom_value_fn = quote::quote! {
+        fn into_idiom_value(self) -> Vec<(surrealdb::sql::Idiom, surrealdb::sql::Value)> {
+           let mut vec: std::vec::Vec<(surrealdb::sql::Idiom, surrealdb::sql::Value)> = std::vec::Vec::new();
+           #(#field_converters)*
 
-        let field_quotes = ast.fields.iter().map(|field| {
-            let field_name = field.ident.as_ref().unwrap();
-
-            return match field.ty {
-                syn::Type::Path(..) => {
-                    quote::quote! {
-                        vec.push((
-                            surrealdb::sql::Idiom::from(stringify!(#field_name).to_owned()), // field name
-                            surrealdb::sql::Operator::#operator_ident, // operator
-                            surrealdb::sql::Value::from(self.#field_name)) // value
-                        );
-                    }
-                },
-                _ => {
-                    quote::quote! {
-                        vec.push((
-                            surrealdb::sql::Idiom::from(stringify!(#field_name).to_owned()), // field name
-                            surrealdb::sql::Operator::#operator_ident, // operator
-                            surrealdb::sql::Value::from(self.#field_name)) // value
-                        );
-                    }
-                }
-            };
-        });
-
-        return quote::quote! {
-            pub fn #fn_ident(self) -> std::vec::Vec<(surrealdb::sql::Idiom, surrealdb::sql::Operator, surrealdb::sql::Value)> {
-               let mut vec: std::vec::Vec<(surrealdb::sql::Idiom, surrealdb::sql::Operator, surrealdb::sql::Value)> = std::vec::Vec::new();
-               #(#field_quotes)*
-
-               return vec;
-           }
-        };
-    }).collect();
+           return vec;
+        }
+    };
 
     let gen = quote::quote! {
-        impl std::convert::Into<std::collections::BTreeMap<String, surrealdb::sql::Value>> for #struct_name {
-            fn into(self) -> std::collections::BTreeMap<String, surrealdb::sql::Value> {
-                let mut map: std::collections::BTreeMap<String, surrealdb::sql::Value> = std::collections::BTreeMap::new();
-                #(#field_convert_btreemap_quote)*
-                return map;
+        impl surreal_devl::serialize::SurrealSerialize for #struct_name {
+            #into_idiom_value_fn
+        }
+
+        impl From<#struct_name> for surrealdb::sql::Value {
+            fn from(value: #struct_name) -> Self {
+                surrealdb::sql::Value::Thing(value.into())
             }
         }
 
-        impl #struct_name {
-           #(#field_convert_set_expressions_quote)*
+        impl From<&#struct_name> for surrealdb::sql::Value {
+            fn from(value: &#struct_name) -> Self {
+               surrealdb::sql::Value::Thing(<#struct_name as Into<surrealdb::sql::Thing>>::into((value.clone())))
+            }
         }
     };
 
     gen.into()
 }
-
-/*
-    surreal_ql! {
-        begin_transaction!()
-        create!(issuer)
-        create!(token)
-        update!()
-        commit_transaction!()
-    }
-*/
